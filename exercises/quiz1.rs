1c18922cf2b028f66f29a25bd918734373fc36e3
meng.wang <meng.w1016@outlook.com>
2025-01-21 17:32:40 +0800

    update

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,17 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
+
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(apples: i32) -> i32 { 
+    let mut price = 2;
+    if apples > 40 {
+        price = 1
+    };
+
+    apples * price
+}
 
 // Don't modify this function!
 #[test]
